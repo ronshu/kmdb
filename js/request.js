5c6725044fbb3c040a1895d3a605a64a74673d98
ronshu <wpwlstm1@gmail.com>
2024-02-01 11:39:46 +0900

    “initial setting02

---
+++ js/request.js
@@ -0,0 +1,23 @@
+/* ====== Common GET Request Function ====== */
+async function getRequest(url) {
+  return await fetch(url).then((response) => {
+    if (!response.ok) {
+      //! : 아니라면
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  });
+}
+
+async function getProducts() {
+  const url = `/kmdb/data/data.php`;
+
+  try {
+    //데이터 요청 및 응답시도 : 성공일 경우 첫번째 코드 블럭으로 이동
+    const data = await getRequest(url);
+    console.log(data);
+  } catch (error) {
+    //실패 할 경우 두번째 코드 블럭으로 이동
+    console.log('Error : ', error);
+  }
+}
